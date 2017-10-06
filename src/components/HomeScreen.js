7a5f8bbe453c61d5373dc8dddac781ed0d2a67e8
NeoS <nvdnvd00@gmail.com>
2017-10-07 00:39:49 +0700

    edit homescreen

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,27 +1,62 @@
 import React, { Component, PropTypes } from "react";
-
+import {
+  Container,
+  Header,
+  Tab,
+  Tabs,
+  ScrollableTab,
+  Left,
+  Right,
+  Button,
+  Icon,
+  Body,
+  Title
+} from "native-base";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-
-
-import MyStackNavigator from "./route";
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
-   
   }
 
   componentDidMount() {}
 
   render() {
     return (
-      <View style={styles.container}>
-
-         <Text>WELCOME</Text>
-      </View>
+      <Container>
+        <Header style={styles.container} hasTabs>
+          <Left>
+            <Button
+              transparent
+              onPress={() => {
+                this.props.navigation.navigate("DrawerOpen");
+              }}
+            >
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Text>Click</Text>
+            </Button>
+          </Right>
+        </Header>
+        <Tabs renderTabBar={() => <ScrollableTab />}>
+          <Tab heading="Tab1" />
+          <Tab heading="Tab2" />
+          <Tab heading="Tab3" />
+          <Tab heading="Tab4" />
+          <Tab heading="Tab5" />
+        </Tabs>
+      </Container>
     );
   }
 }
 const styles = StyleSheet.create({
-  
+  container: {
+    backgroundColor: "#404040"
+  }
 });
